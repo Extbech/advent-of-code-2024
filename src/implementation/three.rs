8dea2d8e4ab5e82a9be5e56c914c409fda84cee8
eqbech <beac@equinor.com>
2024-12-03 11:37:49 +0100

    solution day three

--- src/implementation/three.rs
+++ src/implementation/three.rs
@@ -1,21 +1,45 @@
 use crate::Solution;
-
+use regex::Regex;
 pub struct DayThreeSolution {
-    data: Vec<String>,
+    data: String,
 }
 
 impl Solution for DayThreeSolution {
     const DAY: u8 = 3;
 
     fn new() -> Self {
-        todo!("Implement new function for DayThreeSolution")
+        DayThreeSolution {
+            data: Self::read_data_to_string().unwrap(),
+        }
     }
 
-    fn part_one(&self) -> ! {
-        todo!("Implement part_one function for DayThreeSolution")
+    fn part_one(&self) -> u32 {
+        let re = regex::Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+        re.captures_iter(&self.data)
+            .map(|c| c.extract())
+            .map(|(_, [a, b])| a.parse::<u32>().unwrap() * b.parse::<u32>().unwrap())
+            .sum()
     }
 
-    fn part_two(&self) -> ! {
-        todo!("Implement part_two function for DayThreeSolution")
+    fn part_two(&self) -> u32 {
+        let re = Regex::new(r"do\(\)|don't\(\)|mul\((\d+),(\d+)\)").unwrap();
+        let mut enable_mul = true;
+        let mut sum = 0;
+
+        for cap in re.captures_iter(&self.data) {
+            match cap.get(0).map_or("", |m| m.as_str()) {
+                "do()" => enable_mul = true,
+                "don't()" => enable_mul = false,
+                _ => {
+                    if enable_mul {
+                        if let (Some(a), Some(b)) = (cap.get(1), cap.get(2)) {
+                            sum += a.as_str().parse::<u32>().unwrap()
+                                * b.as_str().parse::<u32>().unwrap();
+                        }
+                    }
+                }
+            }
+        }
+        sum
     }
 }
