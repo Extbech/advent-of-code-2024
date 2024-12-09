c0dfc79cf3c1d8d90efa7b2436bf77e45d7ec162
Extbech <betternerfakali@gmail.com>
2024-12-09 17:18:49 +0100

    progress on day 9

--- src/implementation/nine.rs
+++ src/implementation/nine.rs
@@ -1,21 +1,61 @@
 use crate::Solution;
 
 pub struct DayNineSolution {
-    data: Vec<String>,
+    data: Vec<u32>,
 }
 
 impl Solution for DayNineSolution {
     const DAY: u8 = 9;
 
     fn new() -> Self {
-        todo!("Implement new function for DayNineSolution")
+        DayNineSolution {
+            data: Self::read_data_to_string()
+                .unwrap()
+                .chars()
+                .map(|c| c.to_digit(10).unwrap())
+                .collect::<Vec<u32>>(),
+        }
     }
 
-    fn part_one(&self) -> ! {
-        todo!("Implement part_one function for DayNineSolution")
+    fn part_one(&self) -> u64 {
+        let mut my_vec: Vec<String> = Vec::new();
+        let mut id = 0;
+        for (idx, v) in self.data.iter().enumerate() {
+            if idx % 2 != 0 {
+                my_vec.extend(vec![id.to_string(); *v as usize]);
+                id += 1;
+            } else {
+                my_vec.extend(vec![".".to_string(); *v as usize])
+            }
+        }
+
+        let mut a = 0;
+        let mut b = my_vec.len() - 1;
+        loop {
+            if my_vec[a] == "." {
+                if my_vec[b] != "." {
+                    my_vec.swap(a, b);
+                    a += 1;
+                    b -= 1;
+                } else {
+                    b -= 1;
+                }
+            } else {
+                a += 1;
+            }
+            if my_vec[a..my_vec.len()].iter().all(|s| s == ".") {
+                println!("{:?}", my_vec[0..a].iter().collect::<Vec<&String>>());
+                break;
+            }
+        }
+        my_vec[0..a]
+            .iter()
+            .enumerate()
+            .map(|(i, s)| i as u64 * s.parse::<u64>().unwrap())
+            .sum()
     }
 
-    fn part_two(&self) -> ! {
-        todo!("Implement part_two function for DayNineSolution")
+    fn part_two(&self) -> u64 {
+        42
     }
 }
