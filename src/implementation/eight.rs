a7a701f17d4b724b00f1a020a59a43e5b67f0b66
Benjamin Chandler <beac@equinor.com>
2024-12-09 16:00:57 +0100

    yes

--- src/implementation/eight.rs
+++ src/implementation/eight.rs
@@ -22,12 +22,32 @@ impl Solution for DayEightSolution {
         let mut marked_positions: HashSet<(usize, usize)> = HashSet::new();
         for antenna in &self.antennas {
             find_num_antinode(&self.grid, antenna, &mut marked_positions);
+            println!("{:?}", marked_positions);
         }
+        visualize_grid(self.grid.clone(), &marked_positions);
         marked_positions.len()
     }
-
+    
     fn part_two(&self) -> u32 {
         32
+    }
+}
+
+fn visualize_grid(mut grid: Vec<Vec<u8>>, valid: &HashSet<(usize, usize)>) {
+    for (index, row) in grid.iter_mut().enumerate() {
+        let iter = valid.iter().filter(|(_x, y)| y == &index);
+        for v in iter {
+            row[v.0] = b'#';
+        }
+    }
+    let mut vis_grid: [[char; 12]; 12] = [[char::default(); 12]; 12];
+    for i in 0..grid.len() {
+        for j in 0..grid[i].len() {
+            vis_grid[i][j] = grid[i][j] as char;
+        }
+    }
+    for row in &vis_grid {
+        println!("{:?}", row);
     }
 }
 
@@ -51,36 +71,63 @@ fn find_num_antinode(grid: &[Vec<u8>], antenna: &u8, marked: &mut HashSet<(usize
             }
         }
     }
-    println!("{}, {:?}", antenna, positions);
     for (i, pos_1) in positions.iter().enumerate() {
         for (j, pos_2) in positions.iter().enumerate() {
             if j != i {
-                let diff = (pos_1.0.abs_diff(pos_2.0), pos_1.1.abs_diff(pos_2.1));
-                if !outside_grid(pos_1, diff, grid) {
-                    marked.insert((pos_1.0 - diff.0, pos_1.1 - diff.1));
+                if pos_1.1 < pos_2.1 {
+                    for anti_pos in get_position(grid, pos_1, pos_2) {
+                        if let Some(anti_pos) = anti_pos {
+                            marked.insert(anti_pos);
+                        }
+                    }
                 }
-                if !outside_grid(pos_2, diff, grid) {
-                    marked.insert((pos_2.0 + diff.0, pos_2.1 + diff.1));
+                else if pos_1.1 > pos_2.1 {
+                    for anti_pos in get_position(grid, pos_2, pos_1) {
+                        if let Some(anti_pos) = anti_pos {
+                            marked.insert(anti_pos);
+                        }
+                    }
+                } else {
+                    if pos_1.0 < pos_2.0 {
+                        for anti_pos in get_position(grid, pos_1, pos_2) {
+                            if let Some(anti_pos) = anti_pos {
+                                marked.insert(anti_pos);
+                            }
+                        }
+                    }
+                    else {
+                        for anti_pos in get_position(grid, pos_2, pos_1) {
+                            if let Some(anti_pos) = anti_pos {
+                                marked.insert(anti_pos);
+                            }
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-fn outside_grid(pos_1: &(usize, usize), diff_2: &(usize, usize), grid: &[Vec<u8>]) -> bool {
-    if pos.0.checked_sub(diff.0).is_none() || pos.1.checked_sub(diff.1).is_none() {
-        return true;
-    } else {
-        if grid[pos.1 - diff.1][pos.0 - diff.0] != b'.' {
-            return true;
-        }
+fn get_position<'a>(grid: &'a [Vec<u8>], pos_top: &'a (usize, usize), pos_bot: &'a (usize, usize)) -> Vec<Option<(usize, usize)>> {
+    let diff = (pos_top.0.abs_diff(pos_bot.0), pos_top.1.abs_diff(pos_bot.1));
+    if !is_outside_grid(grid, pos_top, &diff) && !is_outside_grid(grid, pos_bot, &diff){
+        return vec![Some((pos_top.0 - diff.0, pos_top.1 - diff.1)), Some((pos_bot.0 + diff.0, pos_bot.1 + diff.1))];
     }
-    if pos.0 + diff.0 > grid[0].len() - 1 || pos.1 + diff.1 > grid.len() - 1 {
+    if !is_outside_grid(grid, pos_bot, &diff) {
+        return vec![Some((pos_bot.0 + diff.0, pos_bot.1 + diff.1)), None];
+    }
+    if !is_outside_grid(grid, pos_top, &diff) {
+        return vec![Some((pos_top.0 - diff.0, pos_top.1 - diff.1)), None];
+    }
+    vec![None, None]
+}
+
+fn is_outside_grid(grid: &[Vec<u8>], pos: &(usize, usize), diff: &(usize, usize)) -> bool {
+    if (pos.0 as i32 - diff.0 as i32) < 0 || (pos.1 as i32 - diff.1 as i32) < 0 {
         return true;
-    } else {
-        if grid[pos.1 + diff.1][pos.0 + diff.0] != b'.' {
-            return true;
-        }
+    }
+    if pos.0 + diff.0 > grid[0].len() || pos.1 + diff.1 > grid.len() {
+        return true;
     }
     false
 }
