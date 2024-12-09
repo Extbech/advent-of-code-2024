04963536b44390d9ad8a3044f442c49c047f891d
oskgo <92018610+oskgo@users.noreply.github.com>
2024-12-09 16:12:01 +0100

    speedup day 7 (#15)

--- src/implementation/seven.rs
+++ src/implementation/seven.rs
@@ -31,7 +31,7 @@ impl Solution for DaySevenSolution {
     fn part_two(&self) -> u64 {
         let mut res = 0;
         for (input, result) in zip(&self.input, &self.result) {
-            if equation_is_valid_2(input, *result) {
+            if equation_is_valid2(input, *result) {
                 res += result;
             }
         }
@@ -57,28 +57,61 @@ fn parse_input(input: Vec<String>) -> (Vec<u64>, Vec<Vec<u64>>) {
 }
 
 fn equation_is_valid(input: &[u64], result: u64) -> bool {
-    equation_is_valid_req(&input[1..], result, input[0])
+    if input.len() == 1 {
+        return result == input[0];
+    }
+    let m = input.len() - 1;
+    if result % input[m] == 0 && equation_is_valid(&input[..m], result / input[m]) {
+        return true;
+    }
+    if input[m] <= result && equation_is_valid(&input[..m], result - input[m]) {
+        return true;
+    }
+    false
 }
 
-fn equation_is_valid_req(input: &[u64], result: u64, accumulator: u64) -> bool {
+fn equation_is_valid2(input: &[u64], result: u64) -> bool {
+    let ans1 = equation_is_valid2_optimized(input, result);
+    debug_assert_eq!(
+        ans1,
+        equation_is_valid2_unoptimized(&input[1..], result, input[0]),
+        "{} : {:?}",
+        result,
+        input
+    );
+    ans1
+}
+
+fn equation_is_valid2_optimized(input: &[u64], result: u64) -> bool {
+    if input.len() == 1 {
+        return result == input[0];
+    }
+    let m = input.len() - 1;
+    if result % input[m] == 0 && equation_is_valid2_optimized(&input[..m], result / input[m]) {
+        return true;
+    }
+    if result % 10u64.pow(input[m].ilog10() + 1) == input[m]
+        && equation_is_valid2_optimized(&input[..m], result / 10u64.pow(input[m].ilog10() + 1))
+    {
+        return true;
+    }
+    if input[m] <= result && equation_is_valid2_optimized(&input[..m], result - input[m]) {
+        return true;
+    }
+    false
+}
+
+fn equation_is_valid2_unoptimized(input: &[u64], result: u64, accumulator: u64) -> bool {
     if input.is_empty() {
         return result == accumulator;
     }
-    equation_is_valid_req(&input[1..], result, accumulator * input[0])
-        || equation_is_valid_req(&input[1..], result, accumulator + input[0])
-}
-
-fn equation_is_valid_2(input: &[u64], result: u64) -> bool {
-    equation_is_valid_req_2(&input[1..], result, input[0])
-}
-
-fn equation_is_valid_req_2(input: &[u64], result: u64, accumulator: u64) -> bool {
-    if input.is_empty() {
-        return result == accumulator;
-    }
-    equation_is_valid_req_2(&input[1..], result, accumulator * input[0])
-        || equation_is_valid_req_2(&input[1..], result, accumulator + input[0])
-        || equation_is_valid_req_2(&input[1..], result, concatenate_int(accumulator, input[0]))
+    equation_is_valid2_unoptimized(&input[1..], result, accumulator * input[0])
+        || equation_is_valid2_unoptimized(&input[1..], result, accumulator + input[0])
+        || equation_is_valid2_unoptimized(
+            &input[1..],
+            result,
+            concatenate_int(accumulator, input[0]),
+        )
 }
 
 fn concatenate_int(a: u64, b: u64) -> u64 {
