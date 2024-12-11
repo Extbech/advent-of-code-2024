dc42664458117680d6344957bc3c6f47d732625c
eqbech <beac@equinor.com>
2024-12-11 14:12:37 +0100

    remove debug

--- src/implementation/ten.rs
+++ src/implementation/ten.rs
@@ -18,7 +18,6 @@ impl Solution for DayTenSolution {
                 if *num == 0 {
                     println!("Found {} at ({}, {})", find_num_trails(&self.map, (x, y)), x, y);
                     sum += find_num_trails(&self.map, (x, y));
-                    return sum;
                 }
             }
         }
@@ -39,39 +38,31 @@ fn parse_input(input: &[String]) -> Vec<Vec<u8>> {
 
 fn find_num_trails(map: &[Vec<u8>], cords: (usize, usize)) -> u32 {
     let mut sum = 0;
-    let mut cumvec = vec![];
-    find_next_step(map, cords, 0, &mut sum, &mut cumvec);
-    for row in cumvec.chunks(9) {
-        println!("{:?}", row);
-    }
+    find_next_step(map, cords, 0, &mut sum);
     return sum;
 }
 
 // Recursive function to find the next step
-fn find_next_step(map: &[Vec<u8>], cords: (usize, usize), num: u8, accumulator: &mut u32, cumvec: &mut Vec<(usize, usize)>) {
+fn find_next_step(map: &[Vec<u8>], cords: (usize, usize), num: u8, accumulator: &mut u32) {
     if num == 9 {
         *accumulator += 1;
         return
     }
     // Move left
     if cords.0 > 0 && map[cords.1][cords.0 - 1] == num + 1 {
-        cumvec.push((cords.0 - 1, cords.1));
-        find_next_step(map, (cords.0 - 1, cords.1), num + 1, accumulator, cumvec);
+        find_next_step(map, (cords.0 - 1, cords.1), num + 1, accumulator);
     }
     // Move right
     if cords.0 < map[0].len() - 1 && map[cords.1][cords.0 + 1] == num + 1 {
-        cumvec.push((cords.0 + 1, cords.1));
-        find_next_step(map, (cords.0 + 1, cords.1), num + 1, accumulator, cumvec);
+        find_next_step(map, (cords.0 + 1, cords.1), num + 1, accumulator);
     }
     // Move up
     if cords.1 > 0 && map[cords.1 - 1][cords.0] == num + 1 {
-        cumvec.push((cords.0, cords.1 - 1));
-        find_next_step(map, (cords.0, cords.1 - 1), num + 1, accumulator, cumvec);
+        find_next_step(map, (cords.0, cords.1 - 1), num + 1, accumulator);
     }
     // Move down
     if cords.1 < map.len() - 1 && map[cords.1 + 1][cords.0] == num + 1 {
-        cumvec.push((cords.0, cords.1 + 1));
-        find_next_step(map, (cords.0, cords.1 + 1), num + 1, accumulator, cumvec);
+        find_next_step(map, (cords.0, cords.1 + 1), num + 1, accumulator);
     }
 }
 
